9f100b20b79de27f1754040bdaee398d120c100c
CERBERVS-VA <hihihi.hh91@gmail.com>
2025-07-02 15:15:50 +0200

    docker-compose added, pre db

--- start_server.py
+++ start_server.py
@@ -2,4 +2,5 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8080)
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8080, reload=True)
+    #take out reload=True later lmao, only for dev
