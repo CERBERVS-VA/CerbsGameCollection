cb85d3624b7daf368ceb26ae0d878e4a64d9018a
Tobi2106 <flecki2106@gmail.com>
2025-07-16 03:19:04 +0200

    frontend build tool implemented. Vite serves, builds and deploys SPA with typescript and scss support. Init setup finished.

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import type { UserConfig, defineConfig } from 'vite'
+// For .env support: https://vite.dev/config/#using-environment-variables-in-config
+
+
+export default {
+    root: "./frontend/src",
+    define: {
+        __APP_VERSION__: JSON.stringify('v1.0.0'),
+    },
+    build: {
+        outDir: "../.dist",
+        emptyOutDir: true
+    },
+    css: {
+        // See https://sass-lang.com/documentation/js-api/interfaces/stringoptions/
+        preprocessorOptions: {
+            scss: {
+                fatalDeprecations: ["1.89.2"]
+            } as any, // prevent static type checking for SassPreprocessorOptions 
+        }
+    }
+} satisfies UserConfig
