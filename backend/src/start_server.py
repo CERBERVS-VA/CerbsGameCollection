a4fbdb567f515001036854d619fb34975b26405c
CERBERVS-VA <hihihi.hh91@gmail.com>
2025-07-17 14:56:09 +0200

    restructured project, migrated to Typescript (tobi will fix)

--- backend/src/start_server.py
+++ backend/src/start_server.py
@@ -2,5 +2,5 @@ import uvicorn
 
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8080, reload=True)
+    uvicorn.run("api.main:app", host="127.0.0.1", port=8080, reload=True)
     #take out reload=True later lmao, only for dev
--- frontend/src/ts/main.ts
+++ frontend/src/ts/main.ts
@@ -1,47 +1,101 @@
-import '../scss/main.scss'
-import typescriptLogo from '/typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+var gameList: HTMLElement = document.getElementById("all") as HTMLElement;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+//defines the Source for the form Data
+const formRadioInput: HTMLFormElement = document.querySelector("form") as HTMLFormElement;
+const formTitleInput: HTMLInputElement = document.getElementById("add-title") as HTMLInputElement;
+const formUserInput: HTMLInputElement = document.getElementById("add-name") as HTMLInputElement;
 
-function getData(status: boolean, name: string, username: string): Game {
-  return {status, name, username}
+//reads Data from form Inputs
+function readForm() {
+  const formStatus = new FormData(formRadioInput);
+  var status: string = "";
+  for (const entry of formStatus){
+      status = entry[1] as string;
+    }
+    if (status == "") {
+      console.log("400: Status cannot be empty");
+    }
+  var formTitle: string = formTitleInput.value;
+  var formUser: string = formUserInput.value;
+  return{status, formTitle, formUser}
 }
 
-var funnyTest: string = "Test";
-console.log(funnyTest);
+//queries data from API
+async function getData() {
+  const url = "http://127.0.0.1:8080/games/list";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
 
-type Game = {
-  name: string;
-  status: boolean;
-  username: string;
-};
-
-function doStuff(): void {
-  let {status, title, username} = getData(true, "test", "username");
-  console.log(status)
-  console.log(title)
-  console.log(username)
+    const json = await response.json();
+    return json;
+  } catch (error: any) {
+    console.error(error.message);
+  }
 }
 
-doStuff();
+//sends Elements added to poc list to the database and returns status(plus content??)
+function sendData(formTitle: string, formUser: string, status:string){
+  const bodyData = JSON.stringify({"title": formTitle, "releaseyear": null, "publisher": null, "appid": null, "submittername": formUser, "status": status});
+  const request: Request = new Request("http://127.0.0.1:8080/games", {
+    method: "POST",
+    headers: {"Content-Type" : "application/json"},
+    body: bodyData
+  });
+  fetch(request);
+  const reqStat = new Response(request);    //Tobi will fix <33
+  return reqStat.ok;
+}
 
+//adds Elements from query to list
+function addElement(values: string) {
+    const newElement = document.createElement("li");
+    const newContent = document.createTextNode(values);
+    newElement.append(newContent);
+    gameList.appendChild(newElement);
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+//validates the Input from the Form
+function validateFormData(formTitle: string, formUser: string, status:string) {
+  var validKey = 0;
+
+  // No need to evaluate the if with { } if it is a one liner
+  if(status == "ongoing" || status == "planned" || status == "completed") validKey ++;
+    else console.log("Status Validation failed: empty")
+  if(formTitle !== "") validKey ++;
+    else console.log("Gametitle Validation failed: Title empty")
+  if(formUser !== "") validKey ++;
+    else console.log("User Validation failed: Username empty")
+  
+  return validKey
+}
+
+// @ts-ignore
+//"Add" button functionality for processing form data and adding data to api and such
+function buttonAdd() {
+    const {status, formUser, formTitle} = readForm();
+
+    // Yoda conditions 
+    if(validateFormData(formTitle, formUser, status) != 3) return console.log("Validation failed");        // in Python "if function" checks if something exists, in JS it instead checks if it returns something other than 0 (aka false)
+    var dataSentSuccessfully = sendData(formTitle, formUser, status);
+    
+    if(dataSentSuccessfully != true) return console.log("400: couldn't send Data to API");
+    console.log("200: Game added");
+    addElement(formTitle);
+    formTitleInput.value = "";
+    formUserInput.value = "";
+    //snackbar?? pop up for error/success codes :3
+}
+
+//anonymous function to do shit with data before giving it to multiple functions for example
+getData().then(async data =>
+    {
+        console.log(data);
+        var i = 0;
+        while (i < data.length) {
+          addElement(data[i].title);
+          i++;
+        }
+    });
